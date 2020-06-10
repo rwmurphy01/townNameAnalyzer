8a8c8e71c6b0936a0f08055f205e2f7034006327
Robert Murphy <55152111+rwmurphy01@users.noreply.github.com>
2020-06-10 14:01:33 -0500

    Added documentation

--- townsAndCities.py
+++ townsAndCities.py
@@ -34,8 +34,6 @@ states = [
 "Nevada",
 "New Hampshire",
 "New Jersey",
-]
-newStates = [
 "New Mexico",
 "New York",
 "North Carolina",
@@ -58,54 +56,102 @@ newStates = [
 "Wyoming",
 ]
 
-def getNumTownsWithLetter(stateName, letter):
+def getNumTownsWithLetter(stateName: str, letter:str) -> int:
+    """Fetches the number of towns with a certain letter in a given state
+
+    Retrieves the data from demographics.com by parsing their webpage
+
+    Args:
+        stateName: the string of the state the town is located in 
+        letter: the letter to be counted in the state
+    Returns:
+        int: The number of towns in "stateName" that start with "letter"
+    """
+
+    #There is a webpage for every letter in every state on demographics.com
     url = "https://www." + stateName.lower() + "-demographics.com/counties-cities-that-begin-with-" + letter.upper()
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
 
-    hOneTags = soup.findAll("h1")
+    #Gets all of the <h1> tags since if and only if no town starts with "letter" one of the <h1> tags will contain "No city"
+    hOneTags = soup.findAll("h1") 
     for tag in hOneTags:
         if("No City" in tag.text):
             return 0
     
+    #The website includes links for each town that starts with the given "letter" and they are always located in a <div class="span6">
     firstSpanSix = soup.findAll("div", {"class": "span6"})
     aTags = firstSpanSix[1].findAll("a")
     return len(aTags)
 
+def getTotalTowns(stateName: str) -> int:
+    """Fetches the total number of towns in a given state
 
-def getTotalTowns(stateName):
+    Retrieves the data from demographics.com by parsing their webpage
+
+    Args:
+        stateName: the string of the state the towns are to be counted in
+    Returns:
+        int: The number of total towns in "stateName"
+    """
     url = "https://www." + stateName.lower() + "-demographics.com/cities_by_population"
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     results = soup.findAll('p')
 
+    # Always directly following "Below are " in one of the <p> tags on the page is the count of total towns
     for p in results:
         if("Below are " in p.text):
             index = p.text.find("Below are ")
-            index = index + 10
-            nextSpace = p.text.find(" ", index)
+            index = index + 10 #10 spots away from the B in "Below are" is the beginning of the number
+            nextSpace = p.text.find(" ", index) #Finds the index relative to the beginning of the number where it ends
             return p.text[index: nextSpace]
 
-def getPopulation(stateName):
+def getPopulation(stateName: str) -> int:
+    """Fetches the total population of a given state
+
+    Retrieves the data from demographics.com by parsing their webpage
+
+    Args:
+        stateName: the string of the state in question
+    Returns:
+        int: The population of the state
+    """
+
     url = "https://www." + stateName.lower() + "-demographics.com"
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     results = soup.findAll('li')
 
+    # Always in one of the <li> tags on the page is the population of the state
     for li in results:
         if("population" in li.text):
             index = li.text.find("population")
-            nextSpace = li.text.find(" ", index + 14)
-            return li.text[index + 14: nextSpace]
+            index = index + 14 # The beginning of the number is always 14 spaces away from the p in "population"
+            nextSpace = li.text.find(" ", index) #Locates the end of the number
+            return li.text[index: nextSpace]
 
 def runAllStatesAllLetters(): 
+    """Prints values for the starting letters of towns for all 50 states to text files
 
-    f1 = open("TownsWithLetterAsPercentageOfAllTowns", "a")
-    f2 = open("TownsWithLEtterPerMillionPeople", "a")
+    The function computes the value for each letter in the alphabet:
+         1) The number of towns starting with this letter as a percentage of total towns in the state
+         2) The number of towns starting with this letter per million residents of the state
+    Each of these values is printed to their own text files (2 text files total) 
+    where the first 26 results are for the first state alphabetically
+    and the second 26 results are for the second state alphabetically and so on.
+
+    Args:
+        None
+    Returns:
+        void
+    """
+    f1 = open("TownsWithLetterAsPercentageOfAllTowns", "a") #Opens text file for towns with letter as percent of total towns
+    f2 = open("TownsWithLEtterPerMillionPeople", "a") # Opens text file for towns with letter per million residents
 
     for state in newStates:   
         state = state.replace(" ", "")
-        print(state)
+        print(state) # Prints the state so you can keep track of where the program is at
         stateDataTowns = []
         stateDataPop = []
         for c in ascii_lowercase:
@@ -117,20 +163,24 @@ def runAllStatesAllLetters():
             population = population.replace(",", "")
             population = float(population) / 1000000
 
-            townsWithLetterPerTowns = (float(numTowns) / float(totalTowns)) * 100
+            townsWithLetterPerTowns = (float(numTowns) / float(totalTowns)) * 100 # This number is a percentage so multiple by 100
             townsWithLetterPerPop = float(numTowns) / float(population)
             f1.write("%s\n" % townsWithLetterPerTowns)
             f2.write("%s\n" % townsWithLetterPerPop)
-        print("State " + state)
-        print("DONE!")
-
-    print("DONE")
-    print("DONE")
-    print("DONE")
     f1.close()
     f2.close()
 
 def runForOneStateOneLetter():
+    """ Gets user input and calculates the following two values regarding a letter and a state
+            1) The percentage of towns in that state that start with the letter
+            2) The number of towns per million people that begin with that letter in the given state
+
+    Args:
+        NONE
+    Returns:
+        void
+    """
+
     stateName = input("What state? ")
     letter = input("What letter? ")
     stateName = stateName.replace(" ", "")
@@ -140,17 +190,23 @@ def runForOneStateOneLetter():
     population = getPopulation(stateName)
 
     population = population.replace(",", "")
-    population = float(population) / 1000000
+    population = float(population) / 1000000 #Reduces the population to the number of million people
 
-    townsWithLetterPerTowns = float(numTowns) / float(totalTowns)
+    townsWithLetterPerTowns = (float(numTowns) / float(totalTowns)) * 100 # Percentage, multiply by 100
     townsWithLetterPerPop = float(numTowns) / float(population)
 
 
     print("Total number of towns with the letter " + letter + " in " + stateName + " is " + str(numTowns))
     print("Total number of towns in " + stateName + " is " + totalTowns)
     print("The population of " + stateName + " is " + str(population) + " million")
-    print("The percentage of towns with that letter per total towns is " + str(round(townsWithLetterPerTowns * 100, 3)))
+    print("The percentage of towns with that letter per total towns is " + str(round(townsWithLetterPerTowns, 3)))
     print("The number of towns with that letter per million people is " + str(townsWithLetterPerPop))
+
+"""
+This program generates the data for one state and one letter or for all states and all letters
+    1) The percentage of towns in that state that start with the given letter
+    2) The number of towns starting with the given letter per million people in the given state
+"""
 
 print("Do you want to run program to generate data for all states and letters or just one state and one letter?")
 whichFunction = input("(please enter \"all\" or \"one\"): ")
@@ -158,5 +214,3 @@ if(whichFunction.lower() == "all"):
     runAllStatesAllLetters()
 else:
     runForOneStateOneLetter()
-
-
--- townsAndCitiesAnalyzer.py
+++ townsAndCitiesAnalyzer.py
@@ -1,4 +1,19 @@
 from string import ascii_lowercase
+"""
+This program uses the associated files
+    -TownsWithLetterAsPercentageOfAllTowns.txt 
+        -contains data pertaining to all 50 states for each letter in the alphabet
+        where each number is the percentage of towns in that state that begin with that letter
+    -TownsWithLEtterPerMillionPeople.txt
+        -contains data pertaining to all 50 states for each letter in the alphabet
+        where each number is the number of towns that begin with that letter per million people
+
+    Each file begin with the first state alphabetically (Alabama)
+    and the first letter of the alphabet (a) and proceeds
+    down the alpabet (a-z) with newlines deliminating data for 
+    that state before repeating the process with the next one
+"""
+
 
 states = [
 "Alabama",
@@ -56,14 +71,18 @@ states = [
 f1 = open("TownsWithLetterAsPercentageOfAllTowns", "r")
 f2 = open("TownsWithLEtterPerMillionPeople", "r")
 
-count = 0
-townsAsPercentageOfTowns = []
-townsPerMillionPeople = []
+"""
+[letterCount] Counts the number of lines read, when it reaches 26, data pertaining
+to the current state must have gone a-z
+"""
+letterCount = 0 
+townsAsPercentageOfTowns = [[]] # 2D list, rows correspond to states and colums correspond to letters a-z
+townsPerMillionPeople = [[]] # 2D list, rows correspond to states and colums correspond to letters a-z
 
-currentState = []
+currentState = [] # Will hold the values corresponding to a-z for the current state
 
-for line in f1:
-    currentState.append(line[:-1])
+for line in f1: # Reads and appends the data for all states for percentage of towns starting with a given letter
+    currentState.append(line[:-1]) # Takes the data from the line but chops off the ending newline character
     count += 1
     if(count == 26):
         townsAsPercentageOfTowns.append(currentState)
@@ -72,7 +91,7 @@ for line in f1:
 
 currentState = []
 
-for line in f2:
+for line in f2: # Reads and appends the data for all states for the number of towns per million people that start with a given letter
     currentState.append(line[:-1])
     count += 1
     if(count == 26):
@@ -81,9 +100,18 @@ for line in f2:
         count = 0
 
 def getMaxBoth():
+    """Fetches the maximum values for
+        1) The starting letter and the state it is in for the highest percentage of towns that start with that letter in the U.S
+        2) The starting letter and the state it is in for the highest number of towns that start with that letter per million people
+
+    Args:
+        NONE
+    Returns:
+        void
+    """
     max = 0
     for i in range(0, len(townsAsPercentageOfTowns)):
-        for j in range(0, 26):
+        for j in range(0, 26): # 26 for the number of letters in the alphabet
             if(float(townsAsPercentageOfTowns[i][j]) > max):
                 max = float(townsAsPercentageOfTowns[i][j])
                 state = states[i]
@@ -98,7 +126,18 @@ def getMaxBoth():
                 letter = ascii_lowercase[j]
     print("Max town letter per million people is " + str(letter) + " in " + str(state))
 
-def getFromStateLetter(state, letter):
+def getFromStateLetter(state: str, letter: str):
+    """Prints the following data for a given state and letter
+        1) The percentage of towns in that state that begin with that letter
+        2) The number of towns in that state that begin with that letter per million people
+
+    Args:
+        stateName: the string of the state in question
+        letter: the string of the starting town letter
+    Returns:
+        void
+    """
+
     localStates = [x.lower() for x in states]
     stateIndex = localStates.index(state.lower())
     letterIndex = ascii_lowercase.index(letter.lower())
@@ -106,6 +145,7 @@ def getFromStateLetter(state, letter):
     print("The percentage of towns that start with " + str(letter) + " per total towns in " + str(state) + " is " + str(townsAsPercentageOfTowns[stateIndex][letterIndex]))
     print("The number of towns that start with " + str(letter) + " per million people in " + str(state) + " is " + str(townsPerMillionPeople[stateIndex][letterIndex]))
 
+#Example function calls for this program
 getMaxBoth()
 getFromStateLetter("Illinois", "m")
 getFromStateLetter("Wisconsin", "m")
